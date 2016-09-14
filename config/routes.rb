9b96b590aad6d05dfeb78e7c9ed805b1dd9b2cd4
Bang Nguyen and Matt Carlson <bang+matt@>
2016-09-14 10:16:53 -0700

    create image model; delete users_controller; change router to make devise work properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  devise_for :users
-  root to: "users#index"
+#  resources :users
+  devise_for :users, controllers: {}
+  resources :images
+  root to: "images#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
