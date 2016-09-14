fc7ecf192f6774a8b42c6a00fa2d8e6f212a2e55
Bang Nguyen and Matt Carlson <bang+matt@>
2016-09-14 14:01:35 -0700

    add errors partial; write spec for adding images

---
+++ spec/features/add_image_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "the add an image process" do
+  it "adds a new image" do
+    new_user = User.create(email: 'joe@joe.com', password: 'password')
+    visit new_user_session_path
+    fill_in 'Email', :with => 'joe@joe.com'
+    fill_in 'Password', :with => 'password'
+    click_on 'Log in'
+    click_link 'Add an image'
+    fill_in 'Title', :with => 'Something'
+    fill_in 'Url', :with => 'Something else'
+    click_on 'Create Image'
+    expect(page).to have_content 'Something'
+  end
+
+  it "gives error when no information is entered" do
+    new_user = User.create(email: 'joe@joe.com', password: 'password')
+    visit new_user_session_path
+    fill_in 'Email', :with => 'joe@joe.com'
+    fill_in 'Password', :with => 'password'
+    click_on 'Log in'
+    click_link 'Add an image'
+    click_on 'Create Image'
+    expect(page).to have_content 'Please fix these errors:'
+  end
+end
