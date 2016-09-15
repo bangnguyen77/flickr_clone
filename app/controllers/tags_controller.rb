e0992ecf1689accd77ad1942ba676f83ffe45de7
Bang Nguyen and Matt Carlson <bang+matt@>
2016-09-15 09:10:14 -0700

    implement update tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,27 @@
 class TagsController < ApplicationController
   def index
-    
+
+  end
+
+  def edit
+    @image = Image.find(params[:image_id])
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:image_id])
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      flash[:notice] = "Tag successfully updated!"
+      redirect_to image_path(@image)
+    else
+      flash[:alert] = "Tag did not update!"
+      render :edit
+    end
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
